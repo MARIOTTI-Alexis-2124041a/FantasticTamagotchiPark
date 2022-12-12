bb29142430898313ea6a41e3aa2aa0e2c353e904
Alexis M <alexis.mariotti@etu.univ-amu.fr>
2022-12-12 09:41:15 +0100

    added dark

--- src/Interaction.java
+++ src/Interaction.java
@@ -1,5 +1,6 @@
 package src;
 
+import src.bar.Bar;
 import src.tamagotchi.Tamagotchi;
 
 import java.util.ArrayList;
@@ -9,14 +10,20 @@ public class Interaction {
 
     private ArrayList<ArrayList<Tamagotchi>> allTamagotchiByFammily;
 
+    private Tamagotchi actualTamagotchi;
+
 
     public Interaction() {
+        actualTamagotchi = null;
+
         this.allTamagotchiByFammily = new ArrayList<ArrayList<Tamagotchi>>();
 
         //create and add all tamagotchi
     }
 
     public static void main(String[] args) {
+        Interaction interaction = new Interaction();
+
         String userMessage = new String();
 
         Scanner inputScanner = new Scanner(System.in);
@@ -27,6 +34,11 @@ public class Interaction {
             userMessage = inputScanner.nextLine();
 
             System.out.println(userMessage);
+
+            if (interaction.actualTamagotchi != null){
+
+            }
+            //implement randoms actions of the actual tamagotchi
         }
     }
 }
