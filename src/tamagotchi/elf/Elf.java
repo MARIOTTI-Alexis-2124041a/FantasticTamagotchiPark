a5eead4612fe44d08e62551dd3496b1115f4c94e
clemhen <clementine.henault@etu.univ-amu.fr>
2022-12-12 12:14:07 +0100

    finished class Elf

--- src/tamagotchi/elf/Elf.java
+++ src/tamagotchi/elf/Elf.java
@@ -1,4 +1,31 @@
 package tamagotchi.elf;
 
-public class Elf {
+import tamagotchi.Tamagotchi;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class Elf extends Tamagotchi {
+    private final int earSize; //The size of the Elf's ears, between 10 and 20 cm
+    private final List<String> retortList;
+
+    public Elf(int earSize) {
+        this.earSize = earSize;
+        this.retortList = new ArrayList<>();
+        retortList.add("Oh! It's just you...");
+        retortList.add("You are a very redundant person, that's what kind of person you are.");
+    }
+
+    abstract public String joke(int jokeNum);
+
+    public String sillyRetort() {
+        int randomRetortNum = (int) (Math.random() * retortList.size());
+        return retortList.get(randomRetortNum);
+    }
+
+    public String sayHeHasBiggest() {
+        return ("I possess the biggest ears in the land. They are no less then " + earSize + " cm long.");
+    }
+
+    public abstract String playAnInstrument();
 }
