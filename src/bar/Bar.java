9adeeb30aa9cfdde3ec1622d8f48758c0e8c55ba
clemhen <clementine.henault@etu.univ-amu.fr>
2022-12-12 09:17:50 +0100

    created class bar

--- src/bar/Bar.java
+++ src/bar/Bar.java
@@ -1,4 +1,27 @@
 package src.bar;
 
-public class Bar {
+public abstract class Bar {
+    private static final int max;
+    private int currentValue;
+
+    public int getCurrentValue() {
+        return currentValue;
+    }
+
+    public void increaseBar(int val) {
+        if (currentValue + val > max) {
+            currentValue = max;
+        } else {
+            currentValue += val;
+        }
+    }
+
+    /**
+     * Returns true if the current value is considered vital (= is under a certain level)
+     * @return boolean
+     */
+    public abstract boolean isVital();
+
+    public abstract String displayBar();
+
 }
