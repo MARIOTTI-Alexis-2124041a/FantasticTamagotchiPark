97046b5ac1d2becb76d043a80e12450ff5a5717f
Alexis M <alexis.mariotti@etu.univ-amu.fr>
2022-12-12 09:25:36 +0100

    bases of Interaction class

--- src/Interaction.java
+++ src/Interaction.java
@@ -1,8 +1,30 @@
 package src;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Interaction {
 
-    public static void main(String[] args) {
+    //private ArrayList<ArrayList<Tamagotchi>> allTamagotchiByFammily;
 
+
+    public Interaction() {
+        //this.allTamagotchiByFammily = new ArrayList<ArrayList<Tamagotchi>>();
+
+        //create and add all tamagotchi
+    }
+
+    public static void main(String[] args) {
+        String userMessage = new String();
+
+        Scanner inputScanner = new Scanner(System.in);
+
+        System.out.println("----------------------------------------\n--Welcome to FANTASTIC TAMAGOTCHI PARK--\n----------------------------------------\n\ntype \"help\" to know what you can do");
+
+        while (! userMessage.equals("exit")){
+            userMessage = inputScanner.nextLine();
+
+            System.out.println(userMessage);
+        }
     }
 }
